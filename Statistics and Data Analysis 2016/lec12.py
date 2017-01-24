6d301de2c15085d7a5003f22bda831d205fbf799
Steven Karas <steven.karas@gmail.com>
2017-01-24 22:41:27 +0200

    fix lecture 12 code

--- Statistics and Data Analysis 2016/lec12.py
+++ Statistics and Data Analysis 2016/lec12.py
@@ -11,18 +11,17 @@ robfdr = [min(fdr[i:]) for i in range(len(fdr))]
 
 import matplotlib.pyplot as plt
 
-# # Overabundance plot
-# plt.scatter([t / 500.0 for t in range(500)], list(range(500)), c='black', marker='.')
-# plt.scatter(tosses, list(range(len(tosses))))
-# plt.show()
+# Overabundance plot
+plt.scatter([t / 500.0 for t in range(500)], list(range(500)), c='black', marker='.')
+plt.scatter(tosses, list(range(len(tosses))))
+plt.show()
 
-# # FDR plot
-# plt.scatter(list(range(len(fdr))), fdr, c='r', marker='x')
-# # RobFDR plot
-# plt.scatter(list(range(len(robfdr))), robfdr, c='g', marker='x')
-# plt.axhline(y = 0.05)
-# plt.show()
-
+# FDR plot
+plt.scatter(list(range(len(fdr))), fdr, c='r', marker='x')
+# RobFDR plot
+plt.scatter(list(range(len(robfdr))), robfdr, c='g', marker='x')
+plt.axhline(y = 0.05)
+plt.show()
 
 # Car factory data
 german_scores = [3.2, 4.5, 8.1, 9.9, 4.1, 7.3, 5.2, 6.0,]
